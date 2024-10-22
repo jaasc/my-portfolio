df0917d4b3123d1850dbd227b6208423f9979d2e
Jasmine <104405058+jaasc@users.noreply.github.com>
2024-10-22 10:18:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useRef } from "react";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
-import Nav from "./components/Nav";
 import Projects from "./components/Projects";
 
 function App() {
@@ -11,9 +9,9 @@ function App() {
 
   return (
     <div className="font-openSans scroll-smooth bg-[#202c37] text-white">
-      <Nav aboutRef={aboutRef} projectRef={projectRef}/>
+      {/*<Nav aboutRef={aboutRef} projectRef={projectRef}/>*/}
       <Hero/>
-      <About aboutRef={aboutRef}/>
+      {/*<About aboutRef={aboutRef}/>*/}
       <Projects projectRef={projectRef}/>
       <Contact/>
     </div>
